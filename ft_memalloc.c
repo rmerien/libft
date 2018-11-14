0c18eff53100deb13835b308a7d4685c22291c2e
rmerien <merienronan@gmail.com>
2018-11-14 06:22:28 +0100

    aja

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: rmerien <rmerien@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/24 00:55:37 by rmerien           #+#    #+#             */
-/*   Updated: 2018/11/10 13:47:50 by rmerien          ###   ########.fr       */
+/*   Updated: 2018/11/14 06:22:18 by rmerien          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ void	*ft_memalloc(size_t size)
 	void	*mem;
 
 	if (!(mem = malloc(size)))
-		return (NULL);
+		return (0);
 	return (ft_memset(mem, 0, size));
 }
